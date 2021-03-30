d402f0b48c9ad6c82d98e86be62339edccb53699
Noname1180 <Akela.Klimov@yandex.ru>
2021-03-30 17:24:54 +0700

    Спринт-0 завершен

--- S_0/src/Main.java
+++ S_0/src/Main.java
@@ -2,6 +2,7 @@ import gui.ConnectionFrame;
 
 public class Main {
     public static void main(String[] args) {
+
         ConnectionFrame connectionFrame = new ConnectionFrame();
         connectionFrame.singIn();
     }
