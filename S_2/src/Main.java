b70931ce4c850cd47a5395e2775e411650a918cc
Noname1180 <Akela.Klimov@yandex.ru>
2021-04-03 11:17:31 +0700

    изменен внешний вид конопк и приложений

--- S_2/src/Main.java
+++ S_2/src/Main.java
@@ -1,8 +1,15 @@
 import gui.signIn.ConnectionFrame;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception exception){
+            exception.printStackTrace();
+        }
         ConnectionFrame connectionFrame = new ConnectionFrame();
         connectionFrame.singIn();
     }
