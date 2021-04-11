eeca60f30c593b96ed7e3a98351ca7ad8e775b80
Noname1180 <Akela.Klimov@yandex.ru>
2021-04-11 19:46:14 +0700

    Задействован интерфейс по умолчанию

--- S_2/src/Main.java
+++ S_2/src/Main.java
@@ -1,15 +1,7 @@
 import gui.signIn.ConnectionFrame;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        try{
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Exception exception){
-            exception.printStackTrace();
-        }
         ConnectionFrame connectionFrame = new ConnectionFrame();
         connectionFrame.singIn();
     }
