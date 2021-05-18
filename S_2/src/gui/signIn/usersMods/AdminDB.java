0f54d007cbc06682282e717ebf043ee64984ae52
Noname1180 <Akela.Klimov@yandex.ru>
2021-05-18 12:30:06 +0700

    Добавлена таблица пользователей, разделены аминиитсратор БД и приложения, расширена авторизация для всех пользователей

--- S_2/src/gui/signIn/usersMods/AdminDB.java
+++ S_2/src/gui/signIn/usersMods/AdminDB.java
@@ -6,13 +6,11 @@ import gui.UserMods;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class Admin extends UserMod{
-    public Admin(String nameServer, Properties properties, String url) {
+public class AdminDB extends UserMod{
+    public AdminDB(String nameServer, Properties properties, String url) {
         super(nameServer, properties, url);
     }
 
@@ -25,15 +23,16 @@ public class Admin extends UserMod{
     public void openSecurityCheckWindow() {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
-        this.setBounds(dimension.width/2 - 150, dimension.height/2 - 150, 300, 300);
-        this.setTitle("Вход как администратор");
+        this.setBounds(dimension.width/2 - 150, dimension.height/2 - 120, 300, 240);
+        this.setTitle("Вход как администратор БД");
 
         JPanel panel = new JPanel();
         SpringLayout layout = new SpringLayout();
         panel.setLayout(layout);
         this.add(panel);
 
-        JLabel info = new JLabel("<html>Введите ID и пароль<br>администратора</html>");
+
+        JLabel info = new JLabel("<html>Введите пароль<br>администратора БД</html>");
         Font labelFont = info.getFont();
         info.setFont(new Font(labelFont.getName(), Font.PLAIN, 16));
         layout.putConstraint(SpringLayout.WEST, info, 20, SpringLayout.WEST, panel);
@@ -41,29 +40,17 @@ public class Admin extends UserMod{
         layout.putConstraint(SpringLayout.NORTH, info, 20, SpringLayout.NORTH, panel);
         panel.add(info);
 
-        JLabel loginLabel = new JLabel("ID");
-        loginLabel.setFont(new Font(loginLabel.getFont().getName(), Font.PLAIN, 16));
-        layout.putConstraint(SpringLayout.NORTH, loginLabel, 10, SpringLayout.SOUTH, info);
-        layout.putConstraint(SpringLayout.WEST, loginLabel, 20, SpringLayout.WEST, panel);
-        panel.add(loginLabel);
-
-        JTextField login = new JTextField(15);
-        login.setFont(new Font(login.getFont().getName(), Font.PLAIN, 16));
-        layout.putConstraint(SpringLayout.NORTH, login, 10, SpringLayout.SOUTH, loginLabel);
-        layout.putConstraint(SpringLayout.WEST, login, 20, SpringLayout.WEST, panel);
-        panel.add(login);
-
-        JLabel passwordLabel = new JLabel("Пароль");
-        passwordLabel.setFont(new Font(passwordLabel.getFont().getName(), Font.PLAIN, 16));
-        layout.putConstraint(SpringLayout.NORTH, passwordLabel, 10, SpringLayout.SOUTH, login);
-        layout.putConstraint(SpringLayout.WEST, passwordLabel, 20, SpringLayout.WEST, panel);
-        panel.add(passwordLabel);
-
         JPasswordField passwordValue = new JPasswordField(15);
+        passwordValue.setText(getProperties().getProperty("password"));
         passwordValue.setFont(new Font(passwordValue.getFont().getName(), Font.PLAIN, 16));
-        layout.putConstraint(SpringLayout.NORTH, passwordValue, 10, SpringLayout.SOUTH, passwordLabel);
+        layout.putConstraint(SpringLayout.NORTH, passwordValue, 10, SpringLayout.SOUTH, info);
         layout.putConstraint(SpringLayout.WEST, passwordValue, 20, SpringLayout.WEST, panel);
         panel.add(passwordValue);
+
+        JCheckBox initSchema = new JCheckBox("<html>Создать начальную схему<br>(Использовать в случае отладки!)</html>");
+        layout.putConstraint(SpringLayout.NORTH, initSchema, 10, SpringLayout.SOUTH, passwordValue);
+        layout.putConstraint(SpringLayout.WEST, initSchema, 20, SpringLayout.WEST, panel);
+        panel.add(initSchema);
 
         JButton confirm = new JButton("Подтвердить");
         confirm.setFont(new Font(confirm.getFont().getName(), Font.BOLD, 16));
@@ -71,38 +58,30 @@ public class Admin extends UserMod{
         layout.putConstraint(SpringLayout.WEST, confirm, 30, SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.EAST, confirm, -30, SpringLayout.EAST, panel);
         confirm.addActionListener(e -> {
-            String loginValue = login.getText();
-
             StringBuilder pwd = new StringBuilder();
             for(int i = 0; i < passwordValue.getPassword().length; i++){
                 pwd.append(passwordValue.getPassword()[i]);
             }
 
             try {
-                DBConnection connection = new DBConnection(getUrl(), getProperties());
-                PreparedStatement preStatement = connection.getConn().prepareStatement("select count(*) " +
-                        "from USERS where USER_ID = " + loginValue + " and PASSWORD = '" + pwd.toString() + "'" +
-                        " and USER_MOD = 'Администратор'");
-                ResultSet resultSet = preStatement.executeQuery();
-
-                int count = 0;
-                while (resultSet.next()){
-                    count = resultSet.getInt(1);
-                }
-
-                resultSet.close();
-                if(count > 0){
+                if(checkSecurity(pwd.toString(), getProperties().getProperty("password"))){
                     System.out.println("Success!");
                     this.setVisible(false);
-
-                    System.out.println("Set role ADMINISTRATOR");
-                    connection.setRole(UserMods.ADMINISTRATOR);
-
-                    MainWindow mainWindow = new MainWindow(connection, getNameServer(), UserMods.ADMINISTRATOR, false);
+                    boolean debug;
+                    if(initSchema.isSelected()){
+                        System.out.println("Checked");
+                        debug = true;
+                    }
+                    else {
+                        System.out.println("Unchecked");
+                        debug = false;
+                    }
+                    DBConnection connection = new DBConnection(getUrl(), getProperties());
+                    MainWindow mainWindow = new MainWindow(connection, getNameServer(), UserMods.ADMINISTRATOR_DB, debug);
                     mainWindow.run();
                 }
                 else {
-                    throw new SQLException("Неверный ID или пароль администратора!");
+                    throw new SQLException("Неверный пароль администратора БД!");
                 }
             } catch (SQLException exception) {
                 JLabel error = new JLabel("Ошибка подключения! " + exception.getMessage());
@@ -112,10 +91,11 @@ public class Admin extends UserMod{
         });
         panel.add(confirm);
 
+
+
         this.setResizable(false);
         this.setModal(true);
         this.setVisible(true);
     }
-
 
 }
